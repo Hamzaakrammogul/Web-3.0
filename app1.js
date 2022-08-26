e5f0b7d5d041cd1d1b9ac3d680d56c41d7009eeb
Hamza Akram <101792969+Hamzaakrammogul@users.noreply.github.com>
2022-08-26 14:38:12 +0500

    Updated

--- app1.js
+++ app1.js
@@ -1,3 +1,4 @@
+//Inside Ethereum Transaction//
 const Tx = require('ethereumjs-tx').Transaction
 
 const Web3= require('web3');
@@ -6,22 +7,22 @@ const web3 = new Web3('https://goerli.infura.io/v3/f9088ceaa1444035b0ab88ba1f1f0
 const address1= '0xf2aB85a5e4865631230A26EAac4741BA736fB785'
 const privateKey1 =Buffer.from('887f4c88e171b054ab67669b16e99028b28b872675d6cc24cb5b7c52e9c91295', 'hex')
 //console.log(web3.eth.accounts.create())
-const address2= '0x199C4E5A6ECB30080833ba014a0A94295117E273'
-const privateKey2= Buffer.from('0x96e96c6d493ef5395ac22bdd662cf9f11ce5710973dc2d338281f286586ddae5', 'hex')
+const address2= '0x3bD0359740F05CE98739Feaf4EB9B7058d3FDF82'
+const privateKey2= Buffer.from('ad55067ba55005c8dfd0cdd1e423ee7e8b14e03dd05f9658282a0b33fb596444', 'hex')
 web3.eth.getBalance(address1, (err, bal)=>{console.log('address 1 balance:', web3.utils.fromWei(bal, 'ether'))})
 web3.eth.getBalance(address2, (err, bal)=>{console.log('address 2 balance:', web3.utils.fromWei(bal, 'ether'))})
-web3.eth.getTransactionCount(address1, (err, txCount)=>{
+web3.eth.getTransactionCount(address2, (err, txCount)=>{
 //Build the transation
 const txObject={
     nonce: web3.utils.toHex(txCount),
-    to: address2,
-    value:web3.utils.toHex(web3.utils.toWei('1', 'ether')),
+    to: address1,
+    value:web3.utils.toHex(web3.utils.toWei('0.9', 'ether')),
     gasLimit: web3.utils.toHex(21000),
     gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei'))
 }
 //Sign the transaction
 const tx = new Tx(txObject, {chain:'goerli'})
-tx.sign(privateKey1)
+tx.sign(privateKey2)
 
 const serializedTransaction = tx.serialize()
 const raw = '0x' + serializedTransaction.toString('hex')
